e5cace516b3ac6552272138dc35fd7a6eda347b3
abubakr <138801760+abubakr0821@users.noreply.github.com>
2023-11-07 00:02:47 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var buttons = document.querySelectorAll(".drum")
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(){
+        alert("Sen meni bostin")
+    })
+        
+}
